97990437bce6c097b6ba3c3f6cfb11513c54c484
nayeemtomal <80421010+nayeemtomal@users.noreply.github.com>
2021-11-12 22:46:24 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyArKhmblj_IPjrOiTrGf5vBq9Ve0ezvCIk",
+    authDomain: "genius-car-mechanics-52209.firebaseapp.com",
+    projectId: "genius-car-mechanics-52209",
+    storageBucket: "genius-car-mechanics-52209.appspot.com",
+    messagingSenderId: "697636044198",
+    appId: "1:697636044198:web:19af79b8f3a2a94dec2367"
+};
+
+export default firebaseConfig;
